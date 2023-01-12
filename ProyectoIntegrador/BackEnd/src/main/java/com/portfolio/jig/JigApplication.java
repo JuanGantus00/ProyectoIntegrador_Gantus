3a08df5bea6ad632f3a8862914f19de0badef273
JuanGantus00 <122433460+JuanGantus00@users.noreply.github.com>
2023-01-12 00:13:22 -0300

    Creacion Back End

---
+++ ProyectoIntegrador/BackEnd/src/main/java/com/portfolio/jig/JigApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.jig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JigApplication.class, args);
+	}
+
+}
---
+++ ProyectoIntegrador/BackEnd/src/test/java/com/portfolio/jig/JigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.jig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
