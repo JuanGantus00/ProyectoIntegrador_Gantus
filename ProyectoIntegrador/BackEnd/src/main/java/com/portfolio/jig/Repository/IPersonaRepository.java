bddbb5d2f935667275f4015e2f8cd50469a904ec
JuanGantus00 <122433460+JuanGantus00@users.noreply.github.com>
2023-01-12 09:53:51 -0300

    Modificacion BackEnd

---
+++ ProyectoIntegrador/BackEnd/src/main/java/com/portfolio/jig/Repository/IPersonaRepository.java
@@ -0,0 +1,10 @@
+package com.portfolio.jig.Repository;
+
+import com.portfolio.jig.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long> {
+    
+}
